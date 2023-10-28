1eb68a4405d2240dc2d3b7e6a47e28d062d195c7
Deus1704 <jayraj.jayraj@iitgn.ac.in>
2023-10-28 21:43:51 +0530

    made the changes according to the suggestions from Mayank

--- tables/bus_schedule/insert_values.sql
+++ tables/bus_schedule/insert_values.sql
@@ -1,4 +1,13 @@
+-- Insert bus stops with additional parameters
+INSERT INTO bus_stops ("name","distance_from_iitgn")
+VALUES
+    ('IITGN', 0.0),
+    ('Rakshashakti Circle', 2.5),
+    ('VGEC Gate', 5.2),
+    ('Visat Circle', 7.8);
+
+-- Insert bus schedules with JSON data for 'via' using the new structure
 INSERT INTO bus_schedule
     ("from_location", "to_location", "departure_time", "via", "capacity")
 VALUES
-    ('Palaj', 'Visat Circle', '8:00:00', 'IITGN ― Rakshashakti Circle ― VGEC Gate―Visat Circle', '56'),
+    (1, 4, '8:00:00', '[{"stop": 1, "arrival_time": "08:00:00"}, {"stop": 2, "arrival_time": "08:20:00"}, {"stop": 3, "arrival_time": "08:35:00"}, {"stop": 4, "arrival_time": "08:50:00"}]', 56);
--- tables/bus_schedule/make_table.sql
+++ tables/bus_schedule/make_table.sql
@@ -1,37 +1,48 @@
--- Creating the table bus_schedule with non-reserved column names
-CREATE TABLE bus_schedule (
+-- Creating the table bus_stops with unique IDs and additional parameters
+CREATE TABLE bus_stops (
     id SERIAL PRIMARY KEY,
-    from_location VARCHAR NOT NULL,
-    to_location VARCHAR NOT NULL,
-    departure_time TIME NOT NULL,
-    via VARCHAR NOT NULL,
-    capacity INT NOT NULL
+    name VARCHAR NOT NULL,
+    distance_from_iitgn FLOAT
 );
 
+-- Creating the table bus_schedule with changes to 'via' column and references to 'bus_stops' table
+CREATE TABLE bus_schedule (
+    id SERIAL PRIMARY KEY,
+    from_location INT REFERENCES bus_stops(id) NOT NULL,
+    to_location INT REFERENCES bus_stops(id) NOT NULL,
+    departure_time TIME NOT NULL,
+    via JSONB,
+    capacity INT NOT NULL
+);
 
 -- Create a new function to validate JSON for bus_schedule
 CREATE OR REPLACE FUNCTION validate_json_bus_schedule()
 RETURNS TRIGGER AS $$
 DECLARE
-    via_array JSON; -- Declare a JSON variable for 'via' validation
+    via_array JSONB; -- Declare a JSON variable for 'via' validation
 BEGIN
     -- Validate 'via' JSON field
     via_array := NEW.via;
-    
+
     -- Check if 'via' is an array
-    IF NOT JSON_TYPEOF(via_array) = 'array' THEN
+    IF NOT JSONB_typeof(via_array) = 'array' THEN
         RAISE EXCEPTION 'via must be an array';
     END IF;
 
     -- Loop through the 'via' array and validate each element
-    FOR via_element IN SELECT * FROM JSON_ARRAY_ELEMENTS(via_array)
+    FOR via_element IN SELECT * FROM jsonb_array_elements(via_array)
     LOOP
-        -- Check if each element is a string
-        IF NOT JSON_TYPEOF(via_element) = 'string' THEN
-            RAISE EXCEPTION 'Each element in via array must be a string';
+        -- Check if each element is a JSON object
+        IF NOT JSONB_typeof(via_element) = 'object' THEN
+            RAISE EXCEPTION 'Each element in via array must be a JSON object';
         END IF;
 
-        -- Additional validation rules can be added for via elements here
+        -- Check if 'stop' is an integer and references 'bus_stops' table
+        IF NOT (via_element->>'stop')::integer IS NOT NULL THEN
+            RAISE EXCEPTION 'Each via element must have a valid integer "stop" field';
+        END IF;
+
+        -- Additional validation rules for 'arrival_time' can be added here
     END LOOP;
 
     RETURN NEW;
